91580c73ab769aa89f7d2af298443fbff0339502
TessLry <lerayt@live.fr>
2023-12-11 17:25:29 +0100

    création de l'architecture

---
+++ lib/models/score.dart
@@ -0,0 +1,6 @@
+class Score {
+  final int score;
+  final DateTime date;
+
+  Score({required this.score, required this.date});
+}
---
+++ lib/router.dart
@@ -0,0 +1,9 @@
+import 'package:spotiquiz/ui/screens/home.dart';
+
+class AppRouter {
+  static const String homePage = '/home';
+
+  static final routes = {
+    homePage: (context) => const Home(),
+  };
+}
