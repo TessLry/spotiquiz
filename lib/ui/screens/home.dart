14934a794824073898bd7100178d874142d03cce
TessLry <lerayt@live.fr>
2023-12-16 21:14:15 +0100

    Start homePage

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_sdk/spotify_sdk.dart';
+import 'package:spotiquiz/utils/colors.dart';
+import 'package:spotiquiz/utils/credentials.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  dynamic _artists;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SpotiQuiz'),
-        backgroundColor: Color.fromARGB(255, 30, 215, 96),
-      ),
-      body: Center(
-        child: Text(
-          'Home',
+        appBar: AppBar(
+          title: const Text('SpotiQuiz'),
+          backgroundColor: AppColors.primary,
         ),
-      ),
-    );
+        body: Container(
+          child: ElevatedButton(
+            onPressed: () async {
+              // Initialisation de l'API Spotify
+              await SpotifySdk.connectToSpotifyRemote(
+                clientId: AppCredentials.spotifyId,
+                redirectUrl: AppCredentials.spotifySecret,
+              );
+
+              // Exemple de lecture d'une piste
+              await SpotifySdk.play(
+                spotifyUri:
+                    'spotify:track:4iV5W9uYEdYUVa79Axb7Rh', // Remplacez par l'URI de la piste que vous souhaitez lire
+              );
+            },
+            child: Text('Lire une chanson sur Spotify'),
+          ),
+        ));
   }
 }
