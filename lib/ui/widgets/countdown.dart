d485a0f1a52c66bf5a5148dd2f988bbb9558bcd5
Alexis <alexis@gadbin.com>
2024-01-11 20:24:03 +0100

    Add Spotify SDK dependency and related files

---
+++ lib/ui/widgets/countdown.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+class Coutdown extends StatefulWidget {
+  final int timeLeft;
+  final AnimationController animationController;
+
+  const Coutdown({
+    super.key,
+    required this.timeLeft,
+    required this.animationController,
+  });
+
+  @override
+  State<Coutdown> createState() => _CoutdownState();
+}
+
+class _CoutdownState extends State<Coutdown> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      widget.timeLeft.toString(),
+      style: TextStyle(
+        fontSize: 50 - widget.timeLeft * 5,
+        fontWeight: FontWeight.bold,
+      ),
+    )
+        .animate(controller: widget.animationController)
+        .fadeIn()
+        .slideY()
+        .then()
+        .shake();
+  }
+}
